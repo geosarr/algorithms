30d441ecdbc96dea5c386cf4d7db2a78d2a283df
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-12-02 09:08:07 +0100

    initial commit

---
+++ part_one/src/examples/percolation.rs
@@ -0,0 +1,32 @@
+use lib::{PercolationStats, Algorithm};
+use clap::{Parser};
+
+#[derive(Parser)]
+struct Cli {
+    /// Size of the grid (number of rows or columns)
+    #[arg(short, long)]
+    grid_size: usize,
+
+    /// Number of times percolation runs
+    #[arg(short, long)]
+    n_trials: usize,
+
+    /// Union-Find algorithm
+    #[arg(short, long)]
+    algorithm: Algorithm,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    println!("Running {}", cli.algorithm);
+    let mut percol_stats = PercolationStats::init(
+        cli.grid_size, cli.algorithm, cli.n_trials
+    );
+    percol_stats.compute();
+    println!("\nmean = {}", percol_stats.mean());
+    println!("unbiased standard deviation : {}", percol_stats.stddev());
+    let ci = percol_stats.conf_interval();
+    println!("confidence interval [{}, {}]", ci["low"], ci["up"]);
+}
+
