14364fbabb6e189a105ced6e1171a3d24ff7262b
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-12-05 11:55:17 +0100

    added vecstack

--- part_one/src/stack.rs
+++ part_one/src/stack.rs
@@ -1,4 +1,5 @@
 // use std::collections::LinkedList;
+use std::mem::replace;
 
 // fn unbox(value: Box) -> T {
 //     *value
@@ -13,12 +14,12 @@ struct Node {
 }
 
 #[derive(Default, Clone)]
-pub struct Stack {
+pub struct LinkedListStack {
     first: Option<Box<Node>>,
 }
 
 
-impl Stack {
+impl LinkedListStack {
     pub fn new() -> Self {
         Default::default()
     }
@@ -53,6 +54,57 @@ impl Stack {
         let new_node = Node {item: s, next: oldfirst};
         self.first = Some(Box::new(new_node));
     }
+}
 
 
+pub struct VecStack<T>{
+    // Number of not None values in vec
+    n: isize,
+    // Contains the objects
+    pub vec: Vec<Option<T>>, 
+}
+
+impl<T> VecStack<T> {
+    pub fn new() -> Self {
+        Self {
+            n: 0,
+            vec: Vec::new()
+        }
+    }
+
+    pub fn from(capacity: usize) -> Self {
+        let mut vector = Vec::new();
+        for _ in 0..capacity {
+            vector.push(None);
+        }
+
+        Self {
+            n: 0,
+            vec: vector,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.n == 0
+    }
+
+    pub fn push(&mut self, obj: T) {
+        let n = self.n;
+        if n < self.vec.len() {
+            // The stack is not full yet
+            if n > 0 {
+            let _ = replace(&mut self.vec[self.n - 1], Some(obj));}
+            self.n += 1;
+        } else {
+            panic!("cannot push, stack is full");
+        }
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.n > 0 && self.n < self.vec.len() {
+            let elt = replace(&mut self.vec[self.n - 1], None);
+            self.n -= 1;
+            elt
+        } else {panic!("cannot pop, stack has no object.");}
+    }
 }
