9948bd9d5d9cf8b785200721cc7fcc33d597880e
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-12-09 00:25:42 +0100

    usage of linkedlist std

--- part_one/src/queue.rs
+++ part_one/src/queue.rs
@@ -1,94 +1,39 @@
 #[cfg(test)]
 mod unit_test;
-// use std::mem::replace;
 
+use std::collections::LinkedList;
 
-#[derive(Debug, Clone)]
-struct Node {
-    item: String,
-    // Box helps avoid infinity memory allocation
-    // in a recursive definition of a struct
-    next: Option<Box<Node>>,
-}
 
 #[derive(Default, Clone, Debug)]
-pub struct LinkedListQueue {
-    first: Option<Box<Node>>,
-    last: Option<Box<Node>>,
+pub struct LinkedListQueue<T> {
+    list: LinkedList<T>,
 }
 
-
-impl LinkedListQueue {
+impl<T> LinkedListQueue<T>{
     pub fn new() -> Self {
-        Default::default()
-    }
-
-    pub fn init(s: String) -> Self {
-        let node = Node {item: s, next: None};
-        Self {
-            first: Some(Box::new(node.clone())),
-            last: Some(Box::new(node.clone())),
+        Self{
+            list: LinkedList::new(),
         }
+    }
+    
+    pub fn init(s: T) -> Self {
+        let mut res = Self{
+            list: LinkedList::new(),
+        };
+        res.enqueue(s);
+        res
     }
 
     pub fn is_empty(&self) -> bool {
-        match self.first {
-            Some(_) => false,
-            None => true
-        }
+        self.list.is_empty()
     }
 
-    pub fn dequeue(&mut self) -> Option<String> {
-        match self.first{
-            Some(ref node) => {
-                let item = node.item.clone();
-                self.first = node.next.clone();
-                if let None = self.first {
-                    // When the first element in the queue is None
-                    // there is no element in the queue, so last
-                    // should be None as well
-                    self.last = None;
-                }
-                Some(item)
-            },
-            None => panic!("cannot dequeue, queue is empty"), 
-        }
+    pub fn dequeue(&mut self) -> Option<T> {
+        self.list.pop_front()
     }
 
-    pub fn enqueue(&mut self, s: String) {
-        match self.first {
-            None => {
-                // The queue is empty last = None = first
-                // So they should be both equal to node defined as follows
-                let new_node =  Node {item: s, next: None};
-                self.first = Some(Box::new(new_node.clone()));
-                self.last = Some(Box::new(new_node.clone()));
-            },
-            Some(ref mut node) => {
-                if let None = node.next {
-                    let new_node = Node {item: s, next: self.last.clone()};
-                    node.next = Some(Box::new(new_node));
-                } else {
-                    let mut oldlast = self.last.clone();
-                    let newlast = Node {item: s, next: None};
-                    self.last = Some(Box::new(newlast.clone()));
-                    if let Some(ref mut node) = oldlast {
-                        node.next = self.last.clone();
-                    }
-                    self.first = oldlast;                }
-              
-            }
-        } 
-        // else {
-        //     if let Some(ref node) = self.first.next {
-                
-        //     }
-        //     let mut oldlast = self.last.clone();
-        //     let newlast = Node {item: s, next: None};
-        //     self.last = Some(Box::new(newlast.clone()));
-        //     if let Some(ref mut node) = oldlast {
-        //         node.next = self.last.clone();
-        // }}
-        
+    pub fn enqueue(&mut self, element: T) {
+        self.list.push_back(element)
     }
+    
 }
--- part_one/src/queue/unit_test.rs
+++ part_one/src/queue/unit_test.rs
@@ -2,77 +2,35 @@
 mod tests{
     use super::super::super::*;
 
-    fn unbox<T>(value: Box<T>) -> T {
-        *value
+    #[test]
+    fn test_linked_list_queue_new(){
+        let queue = LinkedListQueue::<usize>::new();
+        assert!(queue.is_empty());
     }
 
     #[test]
     fn test_linked_list_queue_init(){
         let string = "test".to_string();
-        let queue = LinkedListQueue::init(string.clone());
-        assert_eq!(string, unbox(queue.first.unwrap()).item);
+        let mut queue = LinkedListQueue::init(string.clone());
+        assert_eq!(Some(string), queue.dequeue());
     }
 
     #[test]
-    #[should_panic]
     fn test_linked_list_queue_dequeue(){
-        let string = "test".to_string();
-        let mut queue = LinkedListQueue::init(string.clone());
-        assert_eq!(Some(string.clone()), queue.dequeue()); 
-        queue.dequeue();
+        let num = -1isize;
+        let mut queue = LinkedListQueue::init(num);
+        assert_eq!(Some(num), queue.dequeue()); 
+        assert_eq!(None, queue.dequeue());
     }
 
-    // #[test]
-    // fn test_linked_list_queue_enqueue(){
-    //     let string = "test".to_string();
-    //     let mut queue = LinkedListQueue::new();
-    //     queue.enqueue(string.clone());
-    //     assert_eq!(Some(string.clone()), queue.dequeue());        
-    // }
-
-    // #[test]
-    // fn test_linked_list_queue_is_empty(){
-    //     let mut queue = LinkedListQueue::new();
-    //     assert!(queue.is_empty());
-    //     queue.enqueue("test".to_string());
-    //     assert!(!queue.is_empty());
-    // }
-
-
-    // #[test]
-    // #[should_panic]
-    // fn test_vec_queue_init(){
-    //     let n = 50;
-    //     let queue = Vecqueue::<String>::init(50);
-    //     let none: Option<String> = None;
-    //     assert_eq!(queue.vec.len(), n);
-    //     assert_eq!(false, queue.vec.iter().any(|e| *e != none));
-    //     Vecqueue::<isize>::init(0);
-    // }
-
-    // #[test]
-    // #[should_panic]
-    // fn test_vec_dequeue(){
-    //     let string = "test".to_string();
-    //     let mut queue = LinkedListQueue::init(string.clone());
-    //     assert_eq!(string.clone(), queue.dequeue()); 
-    //     queue.dequeue();
-    // }
-
-    // #[test]
-    // fn test_vec_enqueue(){
-    //     let string = "test".to_string();
-    //     let mut queue = Vecqueue::<String>::new();
-    //     queue.enqueue(string.clone());
-    //     assert_eq!(string.clone(), queue.dequeue().unwrap());        
-    // }
-
-    // #[test]
-    // fn test_vec_is_empty(){
-    //     let mut queue = Vecqueue::<usize>::new();
-    //     assert!(queue.is_empty());
-    //     queue.enqueue(1);
-    //     assert!(!queue.is_empty());
-    // }
-
+    #[test]
+    fn test_linked_list_queue_enqueue(){
+        let string1 = "test".to_string();
+        let string2 = string1.clone(); 
+        let mut queue = LinkedListQueue::new();
+        queue.enqueue(string1.clone());
+        queue.enqueue(string2.clone());
+        assert_eq!(Some(string1.clone()), queue.dequeue());   
+        assert_eq!(Some(string2.clone()), queue.dequeue());      
+    }
 }
