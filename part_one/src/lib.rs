f9c91b72b449de58258addcfaa1c418f0cad2b3c
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-12-15 16:45:36 +0100

    updated

--- part_one/src/lib.rs
+++ part_one/src/lib.rs
@@ -1,8 +1,10 @@
 pub mod evaluation;
+pub mod collinearity;
 pub mod percolation;
 pub mod threesum;
 pub use evaluation::{Evaluation, LinkedListStack, LinkedListQueue, LinkedListDeque};
+pub use collinearity::{MergeSort, MergeSortAlgorithm};
 pub use percolation::union_find::input_output::{read_lines};
-pub use percolation::{PercolationStats, union_find::algorithm::Algorithm};
+pub use percolation::{PercolationStats, UnionFindAlgorithm};
 pub use threesum::{ThreeSum, binary_search};
 
