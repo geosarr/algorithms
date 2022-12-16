47d24d3b3fa21871f3dd415c4116b35b7eb02c3b
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-12-16 18:46:30 +0100

    add complexity + fmt code + generic type

--- part_one/src/threesum/binary_search.rs
+++ part_one/src/threesum/binary_search.rs
@@ -1,18 +1,21 @@
 #[cfg(test)]
 mod unit_test;
 
-
-pub fn binary_search(key: isize, vec: &[isize]) -> isize{
+pub fn binary_search<T>(key: T, vec: &[T]) -> isize
+where
+    T: PartialOrd,
+{
     // Get the index of key in vec if key is in vec
     // vec elements should be ordered in ascending order.
     // This seems to work only if the length of vec is small enough
     // so that conversions from usize to isize work normally
+    // The complexity is O(log(N))
     if vec.len() > 0 {
         let mut high = vec.len() - 1;
         let mut low = 0;
 
         while low <= high {
-            let mid = (high+low) / 2;
+            let mid = (high + low) / 2;
             if vec[mid] < key {
                 low = mid + 1;
             } else if vec[mid] > key && mid > 0 {
@@ -22,17 +25,14 @@ pub fn binary_search(key: isize, vec: &[isize]) -> isize{
                 // when mid=0 (mid-1 not working since mid is usize)
                 // hence the following conditions
                 return high as isize;
-
             } else if vec[mid] > key && mid == 0 && vec[high] != key {
                 return -1isize;
             } else {
                 return mid as isize;
-            }   
+            }
         }
         return -1isize;
     } else {
         return -1isize;
     }
-    
-
 }
