eb2be953a70534390d84e2e57f786b1b3d4c04f8
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-12-19 23:33:31 +0100

    initial commit

---
+++ part_one/src/collinearity/insertion_sort.rs
@@ -0,0 +1,30 @@
+use std::mem::replace;
+
+
+#[derive(Debug, Default)]
+pub struct InsertionSort<T>{
+    pub vec: Vec<T>
+}
+
+impl<T: Default + Clone + PartialOrd> InsertionSort<T>{
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn init(v: Vec<T>) -> Self {
+        Self {vec: v}
+    }
+
+    pub fn sort(&mut self) {
+        let n = self.vec.len();
+        for i in 0..n{
+            let mut j = i;
+            while j > 0 && self.vec[j] < self.vec[j-1] {
+                let val = self.vec[j-1].clone();
+                self.vec[j-1] = replace(&mut self.vec[j], val);
+                j -= 1;
+            }
+        }
+    }
+
+}
