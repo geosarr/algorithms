0314d476f10e36cdae1060d6e84fb9c77a983ba4
Georges SARR <georges@vmi1022186.contaboserver.net>
2022-12-23 23:50:53 +0100

    added FastCollinearPoints

--- basics/src/bin/collinearity.rs
+++ basics/src/bin/collinearity.rs
@@ -1,6 +1,27 @@
+use clap::Parser;
+use basics::{Point, FastCollinearPoints, BruteCollinearPoints};
+
+#[derive(Parser)]
+struct Cli {
+    /// Absolute path to the file with the integers of the problem
+    #[arg(short, long)]
+    file_abs_path: String,
+
+    /// Separator between the integers
+    #[arg(short)]
+    sep: char,
+}
 
 fn main(){
-
+    let cli = Cli::parse();
+    let filename = cli.file_abs_path;
+    let sep = cli.sep;
+    let points = Point::<isize>::from_file_to_vec(filename, sep);
+    // println!("{:?} {:?}", points[0], points[1]);
+    let mut brute_force = FastCollinearPoints::<isize>::init(points);
+    let segments = brute_force.segments();
+    println!("{}", segments.len()); 
+    println!("{:?}", segments);
 }
 
 
--- basics/src/utils/collinearity.rs
+++ basics/src/utils/collinearity.rs
@@ -1,5 +1,6 @@
 use crate::utils::{Point, LineSegment};
 use crate::sort_algorithm::InsertionSort;
+// use std::collections::HashSet;
 
 #[derive(Debug)]
 pub struct BruteCollinearPoints<T> {
@@ -27,6 +28,7 @@ impl<T: ToString + Ord + Clone> BruteCollinearPoints<T> {
         // In this brute force version of finding all line segments
         // we check if 4 Points are collinear by checking if the slopes
         // between one of them and the rest are equal
+        // run time complexity O(N^4)
         if !self.seg.is_none(){}
         else {
             let mut v = Vec::<LineSegment<T>>::new();
@@ -58,6 +60,90 @@ impl<T: ToString + Ord + Clone> BruteCollinearPoints<T> {
                 }
             }
             self.seg = Some(v);
+        }
+        self.seg.clone().expect("Failed to get LineSegments")
+    }
+}
+
+
+
+
+
+#[derive(Debug)]
+pub struct FastCollinearPoints<T> {
+    // List of Points
+    vec: Vec<Point<T>>,
+    // Line segments of 4 points
+    seg: Option<Vec<LineSegment<T>>>,
+}
+
+impl<T> FastCollinearPoints<T>{
+    pub fn init(v: Vec<Point<T>>) -> Self {
+        // make sure that points are different
+        Self { vec: v, seg: None }
+    }
+}
+
+impl<T: ToString + Ord + Clone + std::fmt::Debug> FastCollinearPoints<T> {
+    pub fn number_of_segments(&mut self) -> usize {
+        match &self.seg {
+            None => self.segments().len(),
+            Some(lsegments) => lsegments.len(),
+        }
+    }
+
+    pub fn segments(&mut self) -> Vec<LineSegment<T>> {
+        // run time complexity O(N^2)
+        // may returns many times the same line(with different points)
+        if !self.seg.is_none(){}
+        else {
+            let mut vec = Vec::<LineSegment<T>>::new();
+            let n = self.vec.len();
+            let mut vec_indices = (0..n).collect::<Vec<usize>>();
+            println!("{:?}", vec_indices);
+            for k in 0..n {
+                // build the lines passing throught the k^th point 
+                vec_indices.retain(|&e| e != k);
+                let mut lines_with_point_k = vec_indices
+                    .iter()
+                    .map(|&l| LineSegment::<T>::init(
+                        self.vec[k].clone(), 
+                        self.vec[l].clone(),
+                    ))
+                    .collect::<Vec<LineSegment<T>>>();
+                vec_indices.push(k);
+                // println!("{:?}", vec_indices);
+                println!("{:?}", lines_with_point_k);
+                
+                // ordering the lines with respect to their slope
+                lines_with_point_k.sort();
+                println!("{:?}", lines_with_point_k);
+                
+                // collinear points with point k are consecutive in lines_with_point_k 
+                let mut i = 0;
+                while i < n-1 {
+                    let mut temp_vec = Vec::<Point<T>>::new();
+                    temp_vec.push(lines_with_point_k[i].get_p()); // point k
+                    temp_vec.push(lines_with_point_k[i].get_q());
+                    let mut l = i+1;
+                    while l < n-1 && lines_with_point_k[i].slope() == lines_with_point_k[l].slope() {
+                        temp_vec.push(lines_with_point_k[l].get_q());                        
+                        l += 1;
+                    }
+                    println!("temp_vec = {:?}, l={}", temp_vec, l);
+                    if temp_vec.len() >= 4 {
+                        // a line of at least 4 points is found
+                        temp_vec.sort();
+                        vec.push(LineSegment::<T>::init(temp_vec[0].clone(), temp_vec.pop().unwrap()));
+                    }
+                    println!("vec = {:?}", vec);
+                    i = l;
+                    println!("\n");
+                }
+            }
+            vec.sort();
+            // vec.dedup();
+            self.seg = Some(vec);
         }
         self.seg.clone().expect("Failed to get LineSegments")
     }
