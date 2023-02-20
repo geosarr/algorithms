157cf3ef1fe72c1bfc38ab06b29e2344f785f3bb
geosarr <georgesmbissanes@gmail.com>
2023-02-20 13:20:02 +0100

    Formatted code

--- advanced/src/graph/directed_graph/unit_test.rs
+++ advanced/src/graph/directed_graph/unit_test.rs
@@ -1,15 +1,12 @@
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::super::DirectedGraph;
     use crate::graph::{
-        DepthFirstSearch,
-        BreadthFirstSearch, 
-        TopologicalSort,
-        StrongConnectedComponent
+        BreadthFirstSearch, DepthFirstSearch, StrongConnectedComponent, TopologicalSort,
     };
 
     #[test]
-    fn test_directed_graph(){
+    fn test_directed_graph() {
         let n: usize = 10;
         let mut graph = DirectedGraph::init(n);
         assert_eq!(graph.nb_vertices(), n);
@@ -26,125 +23,121 @@ mod tests{
 
     #[test]
     #[should_panic]
-    fn test_directed_graph_panic1(){
+    fn test_directed_graph_panic1() {
         let n: usize = 2;
         let mut graph = DirectedGraph::init(n);
-        graph.add_edge(4,1);
+        graph.add_edge(4, 1);
     }
 
     #[test]
     #[should_panic]
-    fn test_directed_graph_panic2(){
+    fn test_directed_graph_panic2() {
         let graph = DirectedGraph::new();
         graph.average_degree();
     }
 
     #[test]
-    fn test_dfs(){
+    fn test_dfs() {
         let mut graph = DirectedGraph::init(9);
-        graph.add_edge(0,1);
-        graph.add_edge(0,2);
-        graph.add_edge(0,6);
-        graph.add_edge(0,5);
-        graph.add_edge(6,4);
-        graph.add_edge(4,3);
-        graph.add_edge(5,4);
-        graph.add_edge(5,3);
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 2);
+        graph.add_edge(0, 6);
+        graph.add_edge(0, 5);
+        graph.add_edge(6, 4);
+        graph.add_edge(4, 3);
+        graph.add_edge(5, 4);
+        graph.add_edge(5, 3);
 
-        let mut dfs = DepthFirstSearch::init(graph.nb_vertices,0);
+        let mut dfs = DepthFirstSearch::init(graph.nb_vertices, 0);
         dfs.find_paths(&graph);
-        assert_eq!(dfs.path_to(2), Some(vec![2,0]));
-        assert!(dfs.path_to(4) == Some(vec![4,6,0]) ||
-            dfs.path_to(4) == Some(vec![4,5,0]) ||
-            dfs.path_to(4) == Some(vec![4,3,5,0])
+        assert_eq!(dfs.path_to(2), Some(vec![2, 0]));
+        assert!(
+            dfs.path_to(4) == Some(vec![4, 6, 0])
+                || dfs.path_to(4) == Some(vec![4, 5, 0])
+                || dfs.path_to(4) == Some(vec![4, 3, 5, 0])
         );
     }
 
     #[test]
-    fn test_bfs(){
+    fn test_bfs() {
         let mut graph = DirectedGraph::init(9);
-        graph.add_edge(0,1);
-        graph.add_edge(0,2);
-        graph.add_edge(0,6);
-        graph.add_edge(0,5);
-        graph.add_edge(6,4);
-        graph.add_edge(4,3);
-        graph.add_edge(5,4);
-        graph.add_edge(5,3);
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 2);
+        graph.add_edge(0, 6);
+        graph.add_edge(0, 5);
+        graph.add_edge(6, 4);
+        graph.add_edge(4, 3);
+        graph.add_edge(5, 4);
+        graph.add_edge(5, 3);
 
         let mut bfs = BreadthFirstSearch::init(graph.nb_vertices, 0);
         bfs.find_paths(&graph);
-        assert_eq!(bfs.path_to(2), Some(vec![2,0]));
-        assert!(bfs.path_to(4) == Some(vec![4,6,0]) ||
-            bfs.path_to(4) == Some(vec![4,5,0])
-        );
-        assert_eq!(bfs.path_to(5), Some(vec![5,0]));
+        assert_eq!(bfs.path_to(2), Some(vec![2, 0]));
+        assert!(bfs.path_to(4) == Some(vec![4, 6, 0]) || bfs.path_to(4) == Some(vec![4, 5, 0]));
+        assert_eq!(bfs.path_to(5), Some(vec![5, 0]));
     }
 
-
     #[test]
-    fn test_topological_sort(){
+    fn test_topological_sort() {
         let mut graph = DirectedGraph::init(7);
-        graph.add_edge(0,1);
-        graph.add_edge(0,2);
-        graph.add_edge(0,5);
-        graph.add_edge(1,4);
-        graph.add_edge(3,2);
-        graph.add_edge(3,4);
-        graph.add_edge(3,5);
-        graph.add_edge(3,6);
-        graph.add_edge(5,2);
-        graph.add_edge(6,4);
-        graph.add_edge(6,0);
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 2);
+        graph.add_edge(0, 5);
+        graph.add_edge(1, 4);
+        graph.add_edge(3, 2);
+        graph.add_edge(3, 4);
+        graph.add_edge(3, 5);
+        graph.add_edge(3, 6);
+        graph.add_edge(5, 2);
+        graph.add_edge(6, 4);
+        graph.add_edge(6, 0);
 
         let mut topo = TopologicalSort::init(graph.nb_vertices);
         topo.depth_first_order(&graph);
         // println!("{:?}", topo.reverse_postorder());
         assert!(
-            topo.reverse_postorder() == &vec![4,1,2,5,0,6,3] ||
-            topo.reverse_postorder() == &vec![2,5,4,1,0,6,3] ||
-            topo.reverse_postorder() == &vec![2,4,1,5,0,6,3]                  
+            topo.reverse_postorder() == &vec![4, 1, 2, 5, 0, 6, 3]
+                || topo.reverse_postorder() == &vec![2, 5, 4, 1, 0, 6, 3]
+                || topo.reverse_postorder() == &vec![2, 4, 1, 5, 0, 6, 3]
         );
     }
 
-
     #[test]
-    fn test_strong_connected_components(){
+    fn test_strong_connected_components() {
         let mut graph = DirectedGraph::init(13);
-        graph.add_edge(0,1);
-        graph.add_edge(0,5);
-        graph.add_edge(2,0);
-        graph.add_edge(2,3);
-        graph.add_edge(3,2);
-        graph.add_edge(3,5);
-        graph.add_edge(4,2);
-        graph.add_edge(4,3);
-        graph.add_edge(5,4);
-        graph.add_edge(6,0);
-        graph.add_edge(6,4);
-        graph.add_edge(6,8);
-        graph.add_edge(6,9);
-        graph.add_edge(7,6);
-        graph.add_edge(7,9);
-        graph.add_edge(8,6);
-        graph.add_edge(9,10);
-        graph.add_edge(9,11);
-        graph.add_edge(10,12);
-        graph.add_edge(11,4);
-        graph.add_edge(11,12);
-        graph.add_edge(12,9);
-
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 5);
+        graph.add_edge(2, 0);
+        graph.add_edge(2, 3);
+        graph.add_edge(3, 2);
+        graph.add_edge(3, 5);
+        graph.add_edge(4, 2);
+        graph.add_edge(4, 3);
+        graph.add_edge(5, 4);
+        graph.add_edge(6, 0);
+        graph.add_edge(6, 4);
+        graph.add_edge(6, 8);
+        graph.add_edge(6, 9);
+        graph.add_edge(7, 6);
+        graph.add_edge(7, 9);
+        graph.add_edge(8, 6);
+        graph.add_edge(9, 10);
+        graph.add_edge(9, 11);
+        graph.add_edge(10, 12);
+        graph.add_edge(11, 4);
+        graph.add_edge(11, 12);
+        graph.add_edge(12, 9);
 
         let mut scc = StrongConnectedComponent::init(graph.nb_vertices);
         scc.find_scc(&graph);
-        assert!(scc.connected(0,3).unwrap());
-        assert!(scc.connected(0,2).unwrap());
-        assert!(scc.connected(0,4).unwrap());
-        assert!(scc.connected(0,5).unwrap());
-        assert!(!scc.connected(0,1).unwrap());
-        assert!(scc.connected(9,10).unwrap());
-        assert!(scc.connected(9,11).unwrap());
-        assert!(scc.connected(11,10).unwrap());
-        assert!(!scc.connected(9,8).unwrap());
+        assert!(scc.connected(0, 3).unwrap());
+        assert!(scc.connected(0, 2).unwrap());
+        assert!(scc.connected(0, 4).unwrap());
+        assert!(scc.connected(0, 5).unwrap());
+        assert!(!scc.connected(0, 1).unwrap());
+        assert!(scc.connected(9, 10).unwrap());
+        assert!(scc.connected(9, 11).unwrap());
+        assert!(scc.connected(11, 10).unwrap());
+        assert!(!scc.connected(9, 8).unwrap());
     }
 }
--- advanced/src/graph/undirected_graph/unit_test.rs
+++ advanced/src/graph/undirected_graph/unit_test.rs
@@ -1,14 +1,10 @@
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::super::UndirectedGraph;
-    use crate::graph::{
-        DepthFirstSearch,
-        BreadthFirstSearch, 
-        ConnectedComponent,
-    };
+    use crate::graph::{BreadthFirstSearch, ConnectedComponent, DepthFirstSearch};
 
     #[test]
-    fn test_undirected_graph(){
+    fn test_undirected_graph() {
         let n: usize = 10;
         let mut graph = UndirectedGraph::init(n);
         assert_eq!(graph.nb_vertices(), n);
@@ -25,82 +21,81 @@ mod tests{
 
     #[test]
     #[should_panic]
-    fn test_undirected_graph_panic1(){
+    fn test_undirected_graph_panic1() {
         let n: usize = 2;
         let mut graph = UndirectedGraph::init(n);
-        graph.add_edge(4,1);
+        graph.add_edge(4, 1);
     }
 
     #[test]
     #[should_panic]
-    fn test_undirected_graph_panic2(){
+    fn test_undirected_graph_panic2() {
         let graph = UndirectedGraph::new();
         graph.average_degree();
     }
 
     #[test]
-    fn test_dfs(){
+    fn test_dfs() {
         let mut graph = UndirectedGraph::init(9);
-        graph.add_edge(0,1);
-        graph.add_edge(0,2);
-        graph.add_edge(0,6);
-        graph.add_edge(0,5);
-        graph.add_edge(6,4);
-        graph.add_edge(4,3);
-        graph.add_edge(5,4);
-        graph.add_edge(5,3);
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 2);
+        graph.add_edge(0, 6);
+        graph.add_edge(0, 5);
+        graph.add_edge(6, 4);
+        graph.add_edge(4, 3);
+        graph.add_edge(5, 4);
+        graph.add_edge(5, 3);
 
-        let mut dfs = DepthFirstSearch::init(graph.nb_vertices,0);
+        let mut dfs = DepthFirstSearch::init(graph.nb_vertices, 0);
         dfs.find_paths(&graph);
-        assert_eq!(dfs.path_to(2), Some(vec![2,0]));
-        assert!(dfs.path_to(4) == Some(vec![4,6,0]) ||
-            dfs.path_to(4) == Some(vec![4,5,0]) ||
-            dfs.path_to(4) == Some(vec![4,3,5,0])
+        assert_eq!(dfs.path_to(2), Some(vec![2, 0]));
+        assert!(
+            dfs.path_to(4) == Some(vec![4, 6, 0])
+                || dfs.path_to(4) == Some(vec![4, 5, 0])
+                || dfs.path_to(4) == Some(vec![4, 3, 5, 0])
         );
     }
 
     #[test]
-    fn test_bfs(){
+    fn test_bfs() {
         let mut graph = UndirectedGraph::init(9);
-        graph.add_edge(0,1);
-        graph.add_edge(0,2);
-        graph.add_edge(0,6);
-        graph.add_edge(0,5);
-        graph.add_edge(6,4);
-        graph.add_edge(4,3);
-        graph.add_edge(5,4);
-        graph.add_edge(5,3);
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 2);
+        graph.add_edge(0, 6);
+        graph.add_edge(0, 5);
+        graph.add_edge(6, 4);
+        graph.add_edge(4, 3);
+        graph.add_edge(5, 4);
+        graph.add_edge(5, 3);
 
         let mut bfs = BreadthFirstSearch::init(graph.nb_vertices, 0);
         bfs.find_paths(&graph);
-        assert_eq!(bfs.path_to(2), Some(vec![2,0]));
-        assert!(bfs.path_to(4) == Some(vec![4,6,0]) ||
-            bfs.path_to(4) == Some(vec![4,5,0])
-        );
-        assert_eq!(bfs.path_to(5), Some(vec![5,0]));
+        assert_eq!(bfs.path_to(2), Some(vec![2, 0]));
+        assert!(bfs.path_to(4) == Some(vec![4, 6, 0]) || bfs.path_to(4) == Some(vec![4, 5, 0]));
+        assert_eq!(bfs.path_to(5), Some(vec![5, 0]));
     }
 
     #[test]
-    fn test_connected_components(){
+    fn test_connected_components() {
         let mut graph = UndirectedGraph::init(13);
-        graph.add_edge(0,1);
-        graph.add_edge(0,2);
-        graph.add_edge(0,6);
-        graph.add_edge(0,5);
-        graph.add_edge(6,4);
-        graph.add_edge(4,3);
-        graph.add_edge(5,4);
-        graph.add_edge(5,3);
-        graph.add_edge(7,8);
-        graph.add_edge(9,10);
-        graph.add_edge(9,11);
-        graph.add_edge(9,12);
-        graph.add_edge(11,12);
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 2);
+        graph.add_edge(0, 6);
+        graph.add_edge(0, 5);
+        graph.add_edge(6, 4);
+        graph.add_edge(4, 3);
+        graph.add_edge(5, 4);
+        graph.add_edge(5, 3);
+        graph.add_edge(7, 8);
+        graph.add_edge(9, 10);
+        graph.add_edge(9, 11);
+        graph.add_edge(9, 12);
+        graph.add_edge(11, 12);
 
         let mut cc = ConnectedComponent::init(graph.nb_vertices);
         cc.find_cc(&graph);
         assert_eq!(cc.count(), 3);
-        assert!(cc.connected(0,3).unwrap());
-        assert!(!cc.connected(5,9).unwrap());
+        assert!(cc.connected(0, 3).unwrap());
+        assert!(!cc.connected(5, 9).unwrap());
     }
 }
