5225bd5a902f2ac6a973c99bfd17799a8670d7be
geosarr <georgesmbissanes@gmail.com>
2023-03-11 21:12:06 +0100

    Added Weight trait

--- advanced/src/graph.rs
+++ advanced/src/graph.rs
@@ -11,3 +11,36 @@ pub trait VertexInfo {
     fn vertex_edges(&self, v: &usize) -> Vec<&usize>;
     fn nb_vertices(&self) -> usize;
 }
+
+// Greatly inspired by :
+// https://github.com/s1ck/graph/blob/main/crates/builder/src/index.rs
+pub trait Weight: Copy + std::ops::Add<Output = Self> + Ord{
+    fn zero() -> Self;
+    fn max() -> Self;
+}
+
+macro_rules! impl_weight{
+    ($TYPE:ty) => {
+        impl Weight for $TYPE {
+            fn max() -> Self {
+                <$TYPE>::MAX
+            }
+
+            fn zero() -> Self {
+               0 as $TYPE
+            }
+        }
+    }
+}
+
+impl_weight!(u8);
+impl_weight!(u16);
+impl_weight!(u32);
+impl_weight!(u64);
+impl_weight!(usize);
+
+impl_weight!(i8);
+impl_weight!(i16);
+impl_weight!(i32);
+impl_weight!(i64);
+impl_weight!(isize);
