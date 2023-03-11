53ceca7e3b2704e11270d40352135595b19574b5
geosarr <georgesmbissanes@gmail.com>
2023-03-11 09:57:27 +0100

    Delete VertexNumber

--- advanced/src/graph.rs
+++ advanced/src/graph.rs
@@ -6,12 +6,8 @@ pub use directed_graph::{DirectedGraph, EdgeWeightedDigraph};
 pub use undirected_graph::UndirectedGraph;
 
 /// This trait gives some basic information on vertices
-pub trait VertexInfo: VertexNumber {
+pub trait VertexInfo {
     // fn vertex_edges(&self, v: &usize) -> &HashSet<usize>;
     fn vertex_edges(&self, v: &usize) -> Vec<&usize>;
-    fn nb_vertices(&self) -> usize;
-}
-
-pub trait VertexNumber {
     fn nb_vertices(&self) -> usize;
 }
