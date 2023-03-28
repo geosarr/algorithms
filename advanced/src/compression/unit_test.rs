7a76d19cdf22bb1bb4a1d906addb16dbe86b98a8
Georges SARR <georgesmbissanes@gmail.com>
2023-03-28 14:20:52 +0200

    Updated deps and corrected compression unit_test

--- advanced/src/compression/unit_test.rs
+++ advanced/src/compression/unit_test.rs
@@ -4,26 +4,29 @@ mod tests {
     #[test]
     fn test_compress() {
         let bits = BitVec::from_bitslice(bits![0, 0, 1, 1]);
+        let len = bits.len();
         let run_length = RunLength::init(bits);
         let compressed_bits = run_length.compress();
-        assert_eq!(vec![2, 2], compressed_bits);
+        assert_eq!((vec![2, 2], len), compressed_bits);
 
         let bits = BitVec::from_bitslice(bits![1, 0, 1, 1]);
+        let len = bits.len();
         let run_length = RunLength::init(bits);
         let compressed_bits = run_length.compress();
-        assert_eq!(vec![0, 1, 1, 2], compressed_bits);
+        assert_eq!((vec![0, 1, 1, 2], len), compressed_bits);
 
         let bits = BitVec::from_bitslice(bits![1, 0, 1, 0]);
+        let len = bits.len();
         let run_length = RunLength::init(bits);
         let compressed_bits = run_length.compress();
-        assert_eq!(vec![0, 1, 1, 1, 1], compressed_bits);
+        assert_eq!((vec![0, 1, 1, 1, 1], len), compressed_bits);
     }
 
     #[test]
     fn test_expand() {
         let bits = BitVec::from_bitslice(bits![0, 0, 1, 1]);
         let run_length = RunLength::init(bits);
-        let compressed_bits = run_length.compress();
+        let (compressed_bits, init_len) = run_length.compress();
         let expanded_bits = run_length.expand(compressed_bits, 4);
         assert_eq!(&expanded_bits, run_length.bits());
     }
