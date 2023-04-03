f482dcaaf29977dbcb56cc7e08c959c2fd168e4f
Georges SARR <georgesmbissanes@gmail.com>
2023-04-03 20:21:15 +0200

    Debut boolean retrieval model implementation

--- advanced/examples/search-engine/src/model.rs
+++ advanced/examples/search-engine/src/model.rs
@@ -1,3 +1,13 @@
 pub struct Boolean<'a> {
     query_type: &'a str,
 }
+
+impl<'a> Boolean<'a> {
+    pub fn new() -> Self {
+        Self { query_type: "AND" }
+    }
+    pub fn retrieve<'b, 'c>(query: &'b str) -> Vec<&'c str> {}
+
+    fn intersect_increasing_freq() {}
+    fn intersect() {}
+}
